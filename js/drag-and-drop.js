55b2fc13a4b7d45b453ce54303159f5b81a43ffa
TAMURA Tomohiro <tamura@goga.co.jp>
2017-04-04 21:11:03 +0900

    rebuilding site 2017/04/04 21:21:04

--- js/drag-and-drop.js
+++ js/drag-and-drop.js
@@ -1,21 +1,16 @@
 function setupDragAndDrop() {
-  var containerElem = $('body');
+  var containerElem = $(window);
   var targetElem = $('#dnd-target');
   var mouseDownStream  = Rx.Observable.fromEvent(targetElem, 'mousedown');
   var mouseUpStream    = Rx.Observable.fromEvent(targetElem, 'mouseup');
 
   var targetStartPos = targetElem.position();
-  var mouseEnterStream = Rx.Observable.fromEvent(targetElem, 'mouseenter');
-  var mouseLeaveStream = Rx.Observable.fromEvent(targetElem, 'mouseleave');
+
   // 終了条件を表すストリーム
-  var terminateDndStream = new Rx.Subject();
-  mouseUpStream.subscribe(function(upEv) { terminateDndStream.next(upEv); });
-  // mouseLeaveStream.subscribe(function() {terminateDndStream.next(null);});
-  Rx.Observable.fromEvent(containerElem, 'mouseleave').subscribe(function() {terminateDndStream.next(null);});
-  // mouseEnterStream.merge(mouseLeaveStream)
-  //   .debounceTime(200)
-  //   .filter(function(ev) { return ev.type === 'mouseout'; })
-  //   .subscribe(function() { terminateDndStream.next(null); });
+  var terminateDndStream = Rx.Observable.merge(
+    mouseUpStream,
+    Rx.Observable.fromEvent($(window), 'mouseup')
+  );
 
   // D&D 終了時に元の位置へ戻す
   terminateDndStream.subscribe(function() {
